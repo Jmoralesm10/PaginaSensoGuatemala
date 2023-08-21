efc25f594d2507b7b6728a78fe2b6f6205675eb1
Javier Morales <jmoralesm45@miumg.edu.gt>
2023-08-21 16:10:23 -0600

    Actualizacion

--- CallAjax.js
+++ CallAjax.js
@@ -1,81 +1,102 @@
 GET();
+var HombresGrafica;
+var MujeresGrafica;
+var MayaGrafica;
+var GarifunaGrafica;
+var AfroGrafica;
+var XincaGrafica;
+var LadinoGrafica;
+
+google.charts.load("current", {packages:["corechart"]});
+google.charts.setOnLoadCallback(drawChart);
+function drawChart() {
+  
+  var data = google.visualization.arrayToDataTable([
+    ['Poblacion Total', 'Hours per Day'],
+    ['Hombres',     HombresGrafica],
+    ['Mujeres',      MujeresGrafica]
+  ]);
+
+  var options = {
+    title: 'Porcentaje de hombres y mujeres',
+    is3D: true,
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+  chart.draw(data, options);
+}
+
+google.charts.setOnLoadCallback(MydrawChart);
+
+      function MydrawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Porcentaje Poblacion por Etnia', 'Hours per Day'],
+          ['Maya',     MayaGrafica],
+          ['Garifuna',      GarifunaGrafica],
+          ['Afrodescediente',  AfroGrafica],
+          ['Xinca', XincaGrafica],
+          ['Ladina',    LadinoGrafica]
+        ]);
+
+        var options = {
+          title: 'Porcentaje Poblacion por Etnia'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
+
 function GET (){
-    var id = document.getElementById('id');
-    var url = "https://censopoblacion.azurewebsites.net/API/indicadores/"+id.value+"/999";
+    var url = "https://censopoblacion.azurewebsites.net/API/indicadores/2/999";
     fetch(url)
         .then(response => response.json())
         .then(data => {
             console.log(data);
             var data2 = JSON.parse(data);
+
+            var NombreDep = data2.nombre;
+            document.getElementById('NombreDep').textContent = "Nombre: " + NombreDep;
+
+            var CapDep = data2.capital;
+            document.getElementById('CapDep').textContent = "Capital: " + CapDep;
+
+            var ExtDep = data2.ext_territorial;
+            document.getElementById('ExtDep').textContent = "Extension territorial: " + ExtDep + " Km²";
             
-            var valorCodigo = data2.depto_id;
-            var codigo = document.getElementById('codigo');
-            codigo.textContent = valorCodigo;
-            console.log(valorCodigo);
+            var pobtotal = data2.pob_total;
+            document.getElementById("pob_total").textContent = "Poblacion Total: " + pobtotal;
 
-            var valornombre = data2.nombre;
-            var nombre = document.getElementById('nombre');
-            nombre.textContent = valornombre;
-            console.log(valornombre);
+            var totalhombres = data2.total_sexo_hombre;
+            document.getElementById("totalHombres").textContent = "Poblacion Masculina: " + totalhombres;
 
-            var valorcapital = data2.capital;
-            var capital = document.getElementById('capital');
-            capital.textContent = valorcapital;
-            console.log(valorcapital);
+            var totalmujeres = data2.total_sexo_mujeres;
+            document.getElementById("totalMujeres").textContent = "Poblacion Femenina: " + totalmujeres;
 
-            var valorpoblacion = data2.pob_total;
-            var pob_total = document.getElementById('pob_total');
-            pob_total.textContent = valorpoblacion;
+            HombresGrafica = totalhombres;
+            MujeresGrafica = totalmujeres;
 
-            var valorhombres = data2.total_sexo_hombre;
-            var hombres = document.getElementById('tot_hombres');
-            hombres.textContent = valorhombres;
+            var maya = data2.pob_pueblo_maya;
+            document.getElementById("maya").textContent = "Poblacion Maya: " + maya;
 
-            var valormujeres = data2.total_sexo_mujeres;
-            var mujeres = document.getElementById('tot_mujeres')
-            mujeres.textContent = valormujeres;
+            var garifuna = data2.pob_pueblo_garifuna;
+            document.getElementById("garifuna").textContent = "Poblacion Garifuna: " + garifuna;
+
+            var afrodescendiente = data2.pob_pueblo_afrodescendiente;
+            document.getElementById("afrodescendiente").textContent = "Poblacion afrodescendiente: " + afrodescendiente;
+
+            var xinca = data2.pob_pueblo_xinca;
+            document.getElementById("xinca").textContent = "Poblacion Xinca: " + xinca;
+
+            var ladino = data2.pob_pueblo_ladino;
+            document.getElementById("ladino").textContent = "Poblacion Ladina: " + ladino;
+
+            MayaGrafica = maya;
+            GarifunaGrafica = garifuna;
+            AfroGrafica = afrodescendiente;
+            XincaGrafica = xinca;
+            LadinoGrafica = ladino;
 
         })
-
-}
-
-GETMuni();
-function GETMuni (){
-    var id = document.getElementById('Did');
-    var Mid = document.getElementById('Mid');
-    var url = "https://censopoblacion.azurewebsites.net/API/indicadores/"+id.value+"/"+Mid.value+"";
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            var data2 = JSON.parse(data);
-            
-            var valorCodigo = data2.depto_id;
-            var codigo = document.getElementById('Mcodigo');
-            codigo.textContent = valorCodigo;
-            console.log(valorCodigo);
-
-            var valorid = data2.municipio_id;
-            var id = document.getElementById('Muniid');
-            id.textContent = valorid;
-
-            var valornombre = data2.nombre;
-            var nombre = document.getElementById('Mnombre');
-            nombre.textContent = valornombre;
-            console.log(valornombre);
-
-            var valorpoblacion = data2.pob_total;
-            var pob_total = document.getElementById('Mpob_total');
-            pob_total.textContent = valorpoblacion;
-
-            var valorhombres = data2.total_sexo_hombre;
-            var hombres = document.getElementById('Mtot_hombres');
-            hombres.textContent = valorhombres;
-
-            var valormujeres = data2.total_sexo_mujeres;
-            var mujeres = document.getElementById('Mtot_mujeres')
-            mujeres.textContent = valormujeres;
-
-        })
-
-}
+    }
